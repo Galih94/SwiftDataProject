c7f95d2b159579281e881456c324a4d46b37b020
galih <samudragalih94@gmail.com>
2024-08-29 10:56:38 +0700

    feat(Filter): set filter user with `R` in name

--- SwiftDataProject/SwiftDataProject/ContentView.swift
+++ SwiftDataProject/SwiftDataProject/ContentView.swift
@@ -1,12 +1,45 @@
+//
+//  ContentView.swift
+//  SwiftDataProject
+//
+//  Created by Galih Samudra on 29/08/24.
+//
 
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+    @Query(filter: #Predicate<User> { user in
+        return user.name.contains("R")
+    }, sort: \User.name) var users: [User]
     var body: some View {
-        Text("Hello world")
+        NavigationStack{
+            List(users) { user in
+                Text(user.name)
+            }
+            .navigationTitle("Users")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Add Samples", systemImage: "plus") {
+                        try? modelContext.delete(model: User.self)
+                        let first = User(name: "Ed Sheeran", city: "London", joinDate: .now.addingTimeInterval(86400 * -10))
+                        let second = User(name: "Rosa Diaz", city: "New York", joinDate: .now.addingTimeInterval(86400 * -5))
+                        let third = User(name: "Roy Kent", city: "London", joinDate: .now.addingTimeInterval(86400 * 5))
+                        let fourth = User(name: "Johnny English", city: "London", joinDate: .now.addingTimeInterval(86400 * 10))
+
+                        modelContext.insert(first)
+                        modelContext.insert(second)
+                        modelContext.insert(third)
+                        modelContext.insert(fourth)
+                    }
+                }
+            }
+        }
     }
 }
 
 #Preview {
     ContentView()
 }
+
