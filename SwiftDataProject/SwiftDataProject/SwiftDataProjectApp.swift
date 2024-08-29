0044de6f61ce0813732be8c0cec88549bea9ccc5
galih <samudragalih94@gmail.com>
2024-08-29 09:25:10 +0700

    feat(SwiftData): create model container

--- SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
+++ SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
@@ -5,6 +5,7 @@
 //  Created by Galih Samudra on 29/08/24.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
@@ -13,5 +14,6 @@ struct SwiftDataProjectApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: User.self)
     }
 }
