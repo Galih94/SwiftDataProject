43d5c92fc73c576e79e7548071e0722f48109cc7
galih <samudragalih94@gmail.com>
2024-08-29 09:13:52 +0700

    wip initial push

---
+++ SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Galih Samudra on 29/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
