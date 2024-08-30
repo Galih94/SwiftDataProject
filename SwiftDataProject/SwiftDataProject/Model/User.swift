937728f1725d3f0fe24f9fd2a9ef83ad72615781
galih <samudragalih94@gmail.com>
2024-08-30 13:31:22 +0700

    feat(Model): add default value to SwiftData model  properties

--- SwiftDataProject/SwiftDataProject/Model/User.swift
+++ SwiftDataProject/SwiftDataProject/Model/User.swift
@@ -10,10 +10,10 @@ import SwiftData
 
 @Model
 class User {
-    var name: String
-    var city: String
-    var joinDate: Date
-    @Relationship(deleteRule: .cascade) var jobs = [Job]() /// make sure delete object Job on User deletion, the default is .nulify that will not delete related Job
+    var name: String = "Anonymous"
+    var city: String = "Unknown"
+    var joinDate: Date = Date.now
+    @Relationship(deleteRule: .cascade) var jobs: [Job]? = [Job]() /// make sure delete object Job on User deletion, the default is .nulify that will not delete related Job
     
     init(name: String, city: String, joinDate: Date) {
         self.name = name
@@ -24,8 +24,8 @@ class User {
 
 @Model
 class Job {
-    var name: String
-    var priority: Int
+    var name: String = "None"
+    var priority: Int = 1
     var owner: User?
     
     init(name: String, priority: Int, owner: User? = nil) {
